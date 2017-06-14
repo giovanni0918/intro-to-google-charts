76795174ab87335d1c4d643679ee9a0ae697d31e
Giovanni Orlando <gorlandor@gmail.com>
2017-06-14 00:09:40 -0400

    Adds Initial Project Structure and Static Demo of Usage
    References:
    1. https://developers.google.com/chart/
    2. http://flopreynat.com/blog/make-google-charts-responsive.html

---
+++ IntroToGoogleCharts/Controllers/ChartsController.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace IntroToGoogleCharts.Controllers
+{
+    public class ChartsController : Controller
+    {
+        // GET: Charts
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        // GET: Charts/About
+        public ActionResult About()
+        {
+            return View();
+        }
+    }
+}
---
+++ IntroToGoogleCharts/Models/Chart.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace IntroToGoogleCharts.Models
+{
+    public class Chart
+    {
+        public int ChartID { get; set; }
+        public object Labels { get; set; }
+        public object Options { get; set; }
+    }
+}
